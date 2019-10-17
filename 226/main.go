3cad149ae2c41079950a3c2dcf0074c50db453b6
chaunceyjiang <chaunceyjiang@gmail.com>
2019-10-17 16:00:33 +0800

    增加题解

--- 226/main.go
+++ 226/main.go
@@ -1,11 +1,33 @@
 package main
 
+import "fmt"
 
+type F interface {
+	f()
+}
+
+type S1 struct{}
+
+func (s S1) f() {}
+
+type S2 struct{}
+
+func (s *S2) f() {}
 
 
 
 func main() {
-	
+
+	s1Val := S1{}
+	s1Ptr := &S1{}
+	s2Val := S2{}
+	s2Ptr := &S2{}
+
+	var i F
+	i = s1Val
+	i = s1Ptr
+	i = s2Ptr
+	fmt.Println(i)
 }
 
 
